213377af3d5ebe91037ff8ec96629f42cd5bb65f
Miguel <mundorap2010@gmail.com>
2019-10-26 18:51:57 -0500

    Up

--- lib/utils/dropdown.dart
+++ lib/utils/dropdown.dart
@@ -9,8 +9,7 @@ class SponsorsDropDown extends StatefulWidget {
   final String title;
   final double width;
 
-  SponsorsDropDown(
-      {Key key, this.sponsors, this.action, this.title, this.width})
+  SponsorsDropDown({Key key, this.sponsors, this.action, this.title, this.width})
       : super(key: key);
   final Storage shared = new Storage();
 
@@ -30,31 +29,31 @@ class SponsorsDropDownState extends State<SponsorsDropDown> {
   Widget build(BuildContext context) {
     return new Theme(
         data: Theme.of(context).copyWith(
-          canvasColor: Colors.white,
-        ),
-        child: new Container(
-            color: Colors.white,
-            width: double.infinity,
-            height: 45,
-            child: new Padding(
-              padding: EdgeInsets.all(6.0),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                    value: current,
-                    items: widget.sponsors.map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: value,
-                        child: new Text(value),
-                      );
-                    }).toList(),
-                    hint: Text(widget.title,
-                        style: TextStyle(color: Colors.grey[700])),
-                    onChanged: (String sponsor) {
-                      widget.action(sponsor);
-                      current = sponsor;
-                      setState(() {});
-                    }),
-              ),
-            )));
+      canvasColor: Colors.white,
+    ),
+    child: new Container(
+        color: Colors.white,
+        width: widget.width,
+        height: 45,
+        child: new Padding(
+          padding: EdgeInsets.all(6.0),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+                value: current,
+                items: widget.sponsors.map((String value) {
+                  return new DropdownMenuItem<String>(
+                    value: value,
+                    child: new Text(value),
+                  );
+                }).toList(),
+                hint: Text(widget.title,
+                    style: TextStyle(color: Colors.grey[700])),
+                onChanged: (String sponsor) {
+                  widget.action(sponsor);
+                  current = sponsor;
+                  setState(() {});
+                }),
+          ),
+        )));
   }
 }
