9eb8e5a9be27fa98d12f42d79eee72bd47be81da
Miguel <mundorap2010@gmail.com>
2019-10-25 23:30:29 -0500

    Return face widget

--- lib/nebula_id.dart
+++ lib/nebula_id.dart
@@ -1,13 +1,34 @@
 import 'dart:async';
 
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:nebula_id/machine/face/face_life.dart';
 
 class NebulaId {
-  static const MethodChannel _channel =
-      const MethodChannel('nebula_id');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  Widget faceUUID(
+      List<CameraDescription> cameras,
+      String title,
+      String subtitle,
+      String extra,
+      String toRight,
+      String toLeft,
+      String closeEyes,
+      String takePhoto,
+      OnResultFace resultFace,
+      List<List<Color>> colors) {
+    return new Face(
+      title: title,
+      cameras: cameras,
+      subtitle: subtitle,
+      extra: extra,
+      toRight: toRight,
+      toLeft: toLeft,
+      closeEyes: closeEyes,
+      takePhoto: takePhoto,
+      resultFace: resultFace,
+      colors: colors
+    );
   }
 }
