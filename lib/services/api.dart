3056e569cf7b333b1f527d06490d4db05fa92e2f
Miguel <mundorap2010@gmail.com>
2019-10-26 16:05:47 -0500

    Fix ttl

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -11,7 +11,7 @@ class ApiService implements Service {
 
   ApiService() {
     auth = NebulaId.auth;
-    dio.options.baseUrl = 'https://192.168.0.8:3020/v1/${auth.getCompany()}/';
+    dio.options.baseUrl = 'http://192.168.0.8:3020/v1/${auth.getCompany()}/';
     dio.options.connectTimeout = 10000;
     dio.options.receiveTimeout = 10000;
     (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
